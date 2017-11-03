d1e0dbe619e88302d349d15f3eaf9169ff8beb0f
brittnibegale <brittnibegale@gmail.com>
2017-11-03 09:34:24 -0500

    add the upload page with most functionality besides checking if the info is correct

--- VirtualWellnessProgram/VirtualWellnessProgram/Controllers/HomeController.cs
+++ VirtualWellnessProgram/VirtualWellnessProgram/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LumenWorks.Framework.IO.Csv;
 
 namespace VirtualWellnessProgram.Controllers
 {
@@ -24,6 +27,44 @@ namespace VirtualWellnessProgram.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        Stream stream = upload.InputStream;
+                        DataTable csvTable = new DataTable();
+                        using (CsvReader csvReader = new CsvReader(new StreamReader(stream), true))
+                        {
+                            csvTable.Load(csvReader);
+                        }
+                        return View(csvTable);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
             return View();
         }
     }
